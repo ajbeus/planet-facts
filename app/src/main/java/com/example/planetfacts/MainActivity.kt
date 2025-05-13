7b875d14aeb91dfd748f885692f7493ccfc557aa
ajbeus <168456943+ajbeus@users.noreply.github.com>
2025-05-13 15:02:28 -0600

    Hello World

--- app/src/main/java/com/example/planetfacts/MainActivity.kt
+++ app/src/main/java/com/example/planetfacts/MainActivity.kt
@@ -21,7 +21,7 @@ class MainActivity : ComponentActivity() {
             PlanetFactsTheme {
                 Scaffold(modifier = Modifier.fillMaxSize()) { innerPadding ->
                     Greeting(
-                        name = "Android",
+                        name = "Hello World",
                         modifier = Modifier.padding(innerPadding)
                     )
                 }
